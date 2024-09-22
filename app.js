660d7552b152a41c37e360da6453121cab99d1a1
abdulrafay-1 <abdulrock413@gmail.com>
2024-09-23 00:51:07 +0500

    added previous button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const main = document.getElementById("main");
 const nextBtn = document.getElementById("next-btn");
+const previousBtn = document.getElementById("prev-btn");
 
 let data;
 let index = 0;
@@ -9,8 +10,9 @@ async function fetchUsers() {
     data = await response.json()
     main.innerHTML = `
         <h1>${data[index].name}</h1>
-        <p>${data[index].email}</p>
-        <p>${data[index].phone}</p>
+        <p>Email : ${data[index].email}</p>
+        <p>Phone : ${data[index].phone}</p>
+        <p>Website : ${data[index].website}</p>
     `
 }
 fetchUsers()
@@ -19,7 +21,19 @@ nextBtn.addEventListener('click', () => {
         index = index + 1;
         main.innerHTML = `
         <h1>${data[index].name}</h1>
-        <p>${data[index].email}</p>
-        <p>${data[index].phone}</p>
+        <p>Email : ${data[index].email}</p>
+        <p>Phone : ${data[index].phone}</p>
+        <p>Website : ${data[index].website}</p>
+    `}
+})
+
+previousBtn.addEventListener('click', () => {
+    if (data && (index != 0)) {
+        index = index - 1;
+        main.innerHTML = `
+        <h1>${data[index].name}</h1>
+        <p>Email : ${data[index].email}</p>
+        <p>Phone : ${data[index].phone}</p>
+        <p>Website : ${data[index].website}</p>
     `}
 })
