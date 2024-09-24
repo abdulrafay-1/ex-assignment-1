c2e35f08f60287bbe4ecf9d0485922a748878d68
abdulrafay-1 <abdulrock413@gmail.com>
2024-09-24 23:42:14 +0500

    optimized code using function

--- app.js
+++ app.js
@@ -8,34 +8,31 @@ let index = 0;
 async function fetchUsers() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     data = await response.json()
+    renderUser(index)
+
+}
+
+fetchUsers()
+
+function renderUser(index) {
     main.innerHTML = `
         <h1>${data[index].name}</h1>
         <p>Email : ${data[index].email}</p>
         <p>Phone : ${data[index].phone}</p>
         <p>Website : ${data[index].website}</p>
-    `
+    `;
 }
-
-fetchUsers()
 
 nextBtn.addEventListener('click', () => {
     if (data && (index < data.length - 1)) {
         index = index + 1;
-        main.innerHTML = `
-        <h1>${data[index].name}</h1>
-        <p>Email : ${data[index].email}</p>
-        <p>Phone : ${data[index].phone}</p>
-        <p>Website : ${data[index].website}</p>
-    `}
+        renderUser(index)
+    }
 })
 
 previousBtn.addEventListener('click', () => {
     if (data && (index != 0)) {
         index = index - 1;
-        main.innerHTML = `
-        <h1>${data[index].name}</h1>
-        <p>Email : ${data[index].email}</p>
-        <p>Phone : ${data[index].phone}</p>
-        <p>Website : ${data[index].website}</p>
-    `}
+        renderUser(index)
+    }
 })
